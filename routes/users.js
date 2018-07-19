8e13014e824021b6f4d8f9fc11ea87767fa07aa4
marianar97 <Ramirez@MacBook-Pro-de-Mariana.local>
2018-07-19 15:36:30 -0500

    añadiendo iniciar sesión

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
 let User = require('../models/user');
 
 
@@ -53,4 +56,42 @@ router.post('/register', function(req, res) {
         res.redirect('/users/login');
     }
 });
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        User.getUserByUsername(username, function(err, user) {
+            if (err) throw err;
+            if (!user) {
+                return done(null, false, { message: 'Unknown User' });
+            }
+
+            User.comparePassword(password, user.password, function(err, isMatch) {
+                if (err) throw err;
+                if (isMatch) {
+                    return done(null, user);
+                } else {
+                    return done(null, false, { message: 'Invalid password' });
+                }
+            });
+        });
+    }));
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.getUserById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+
+router.post('/login',
+    passport.authenticate('local', { successRedirect: '/', failureRedirect: '/users/login', failureFlash: true }),
+    function(req, res) {
+        res.redirect('/');
+    });
+
+
 module.exports = router;
