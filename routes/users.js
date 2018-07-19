c94ea9a20851a0878a27a0dfe018cb3a8bd6c61e
marianar97 <Ramirez@MacBook-Pro-de-Mariana.local>
2018-07-19 15:06:46 -0500

    Añadiendo funcionalidad de registrarse

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,56 @@
 const express = require('express');
 const router = express.Router();
 
-// Muestra la página home
+let User = require('../models/user');
+
+
+// Register
 router.get('/register', function(req, res) {
     res.render('register');
 });
 
+// Login
 router.get('/login', function(req, res) {
     res.render('login');
 });
 
+// Register User
+router.post('/register', function(req, res) {
+    var name = req.body.name;
+    var email = req.body.email;
+    var username = req.body.username;
+    var password = req.body.password;
+    var password2 = req.body.password2;
+    console.log(name);
+
+    // Validación
+    req.checkBody('name', 'El nombre es requerido').notEmpty();
+    req.checkBody('email', 'El Email es requerido').notEmpty();
+    req.checkBody('email', 'El Email es no válido').isEmail();
+    req.checkBody('username', 'El username es requerido').notEmpty();
+    req.checkBody('password', 'El password es requerido').notEmpty();
+    req.checkBody('password2', 'Las contraseñas no son iguales').equals(req.body.password);
+
+    var errors = req.validationErrors();
+
+    if (errors) {
+        res.render('register', {
+            errors: errors
+        });
+    } else {
+        let newUser = new User({
+            name: name,
+            email: email,
+            username: username,
+            password: password
+        })
+
+        User.createUser(newUser, function(err, user) {
+            if (err) throw err
+            console.log(user);
+        });
+        req.flash('success_msg', "Te registraste exitosamente");
+        res.redirect('/users/login');
+    }
+});
 module.exports = router;
