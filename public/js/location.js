c5b09adfb461e7024f1eb62d0a4d6259a2ffb6fb
marianar97 <Ramirez@MacBook-Pro-de-Mariana.local>
2018-07-20 14:47:26 -0500

    añadiendo google maps

---
+++ public/js/location.js
@@ -0,0 +1,47 @@
+function success(position) {
+    var s = document.querySelector('#status');
+
+    if (s.className == 'success') {
+        return;
+    }
+
+    s.innerHTML = "found you!";
+    s.className = 'success';
+
+    var mapcanvas = document.createElement('div');
+    mapcanvas.id = 'mapcanvas';
+    mapcanvas.style.height = '400px';
+    mapcanvas.style.width = '560px';
+
+    document.querySelector('article').appendChild(mapcanvas);
+
+    var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    var myOptions = {
+        zoom: 15,
+        center: latlng,
+        mapTypeControl: false,
+        navigationControlOptions: { style: google.maps.NavigationControlStyle.SMALL },
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("mapcanvas"), myOptions);
+
+    var marker = new google.maps.Marker({
+        position: latlng,
+        map: map,
+        title: "You are here! (at least within a " + position.coords.accuracy + " meter radius)"
+    });
+}
+
+function error(msg) {
+    var s = document.querySelector('#status');
+    s.innerHTML = typeof msg == 'string' ? msg : "failed";
+    s.className = 'fail';
+
+    // console.log(arguments);
+}
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(success, error);
+} else {
+    error('not supported');
+}
--- routes/users.js
+++ routes/users.js
@@ -93,5 +93,13 @@ router.post('/login',
         res.redirect('/');
     });
 
+router.get('/logout', function(req, res) {
+    req.logout();
+
+    req.flash('success_msg', 'You are logged out');
+
+    res.redirect('/users/login');
+});
+
 
 module.exports = router;
