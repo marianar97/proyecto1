52fb4392af9c0b082bcee3e2b7cf29f230b6afcd
marianar97 <Ramirez@MacBook-Pro-de-Mariana.local>
2018-08-04 19:46:58 -0500

    añadiendo javascript

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ var LocalStrategy = require('passport-local').Strategy;
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 
-//mongoose.connect('mongodb://localhost/login');
 
 mongoose.connect('mongodb://mongo-server/login');
 var db = mongoose.connection;
